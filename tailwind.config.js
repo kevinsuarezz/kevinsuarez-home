a79605764825435262667a88049d58f35c66e8f0
Kevin <kevinsuarez0010@gmail.com>
2024-12-13 23:30:26 -0500

    Se agrega visualizacion inicial del proyecto, se establece paleta de colores principales

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,17 @@
 /** @type {import('tailwindcss').Config} */
+
 export default {
   content: ['./src/**/*.{astro,html,md,mdx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'black': '#000',
+        'almost-black': '#051819',
+        'blue': '#2cf8ff',
+        'green': '#18ffb0',
+        'light': '#96a7a1',
+      }
+    },
   },
   plugins: [],
 }
-
